33848c2847329c2840bf030acc586541af091c98
Larksel <lemuelgames@outlook.com>
2024-04-24 23:34:31 -0300

    exAgenda completo

--- exAgenda.c
+++ exAgenda.c
@@ -10,9 +10,13 @@ struct Agenda
 };
 
 short menu();
-void listar(struct Agenda *agenda,  int *cont);
+short descobrirIndx(struct Agenda *agenda, int *cont, int cod);
+short gerarCod(struct Agenda *agenda, int *cont);
 void cadastrar(struct Agenda *agenda, int *cont);
+void listar(struct Agenda *agenda,  int *cont);
+void editar(struct Agenda *agenda,  int *cont);
 void excluir(struct Agenda *agenda, int *cont);
+void ordenarNome(struct Agenda *agenda, int *cont);
 
 int main() {
     int cont = 0;
@@ -24,19 +28,19 @@ int main() {
         switch (escolha)
         {
         case 1:
-            listar(agenda, &cont);
-            break;
-        case 2:
             cadastrar(agenda, &cont);
             break;
+        case 2:
+            listar(agenda, &cont);
+            break;
         case 3:
-            excluir(agenda, &cont);
+            editar(agenda, &cont);
             break;
         case 4:
-            printf("Editar\n\n");
+            excluir(agenda, &cont);
             break;
         case 5:
-            printf("Ordenar por nome\n\n");
+            ordenarNome(agenda, &cont);
             break;
         
         default:
@@ -52,16 +56,34 @@ short menu() {
     printf("\n=================================\n");
     printf("              Agenda             \n");
     printf("=================================\n");
-    printf("[1] Listar\n");
-    printf("[2] Cadastrar\n");
-    printf("[3] Excluir\n");
-    printf("[4] Editar\n");
+    printf("[1] Cadastrar\n");
+    printf("[2] Listar\n");
+    printf("[3] Editar\n");
+    printf("[4] Excluir\n");
     printf("[5] Ordenar por nome\n");
     printf("[0] Sair\n");
     printf("-> ");
     scanf("%hd", &escolha);
     printf("\n");
     return escolha;
+}
+
+void cadastrar(struct Agenda *agenda, int *cont) {
+    if (*cont < N) {
+        if (*cont == 0) {
+            agenda[*cont].codigo = 1;
+        } else {
+            agenda[*cont].codigo = gerarCod(agenda, cont);
+        }
+        
+        printf("Digite o nome: ");
+        scanf("%s", agenda[*cont].nome);
+        printf("Digite o telefone: ");
+        scanf("%d", &agenda[*cont].telefone);
+        *cont += 1;
+    } else {
+        printf("A agenda esta cheia.\n");
+    }
 }
 
 void listar(struct Agenda *agenda,  int *cont) {
@@ -79,31 +101,29 @@ void listar(struct Agenda *agenda,  int *cont) {
     }
 }
 
-void cadastrar(struct Agenda *agenda, int *cont) {
-    if (*cont < N) {
-        *cont == 0 ? 
-        (agenda[*cont].codigo = 1) : 
-        (agenda[*cont].codigo = agenda[*cont-1].codigo + 1);
-        printf("Digite o nome: ");
-        scanf("%s", agenda[*cont].nome);
-        printf("Digite o telefone: ");
-        scanf("%d", &agenda[*cont].telefone);
-        *cont += 1;
+void editar(struct Agenda *agenda,  int *cont) {
+    int cod;
+
+    printf("Digite o codigo do registro a ser editado: ");
+    scanf("%d", &cod);
+    short index = descobrirIndx(agenda, cont, cod);
+
+    if (index != -1) {
+        printf("Insira o novo nome: ");
+        scanf("%s", agenda[index].nome);
+        printf("Insira o novo telefone: ");
+        scanf("%d", &agenda[index].telefone);
     } else {
-        printf("A agenda esta cheia.\n");
+        printf("Nao existe um registro com o codigo %d\n", cod);
     }
 }
 
 void excluir(struct Agenda *agenda, int *cont) {
-    short i, index = -1;
+    short i;
     int cod;
     printf("Digite o codigo do registro a ser excluido: ");
     scanf("%d", &cod);
-    for (i = 0; i < *cont; i++) {
-        if (agenda[i].codigo == cod) {
-            index = i;
-        }
-    }
+    short index = descobrirIndx(agenda, cont, cod);
 
     if (index != -1) {
         for (i = index; i < *cont-1; i++) {
@@ -117,4 +137,52 @@ void excluir(struct Agenda *agenda, int *cont) {
     } else {
         printf("Nao existe um registro com o codigo %d\n", cod);
     }
+}
+
+void ordenarNome(struct Agenda *agenda, int *cont) {
+    short i, j;
+    int codTemp;
+    char nomeTemp[30];
+    int telTemp;
+
+    for (i = 0; i < *cont; i++) {
+        for (j = 0; j < *cont - 1; j++) {
+            if (strcmp(agenda[j].nome, agenda[j+1].nome) > 0) {
+                codTemp = agenda[j].codigo;
+                strcpy(nomeTemp, agenda[j].nome);
+                telTemp = agenda[j].telefone;
+
+                agenda[j].codigo = agenda[j+1].codigo;
+                strcpy(agenda[j].nome, agenda[j+1].nome);
+                agenda[j].telefone = agenda[j+1].telefone;
+
+                agenda[j+1].codigo = codTemp;
+                strcpy(agenda[j+1].nome, nomeTemp);
+                agenda[j+1].telefone = telTemp;
+            }
+        }
+    }
+
+    listar(agenda, cont);
+}
+
+short descobrirIndx(struct Agenda *agenda, int *cont, int cod) {
+    short i;
+    for (i = 0; i < *cont; i++) {
+        if (agenda[i].codigo == cod) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+short gerarCod(struct Agenda *agenda, int *cont) {
+    short i, maior = 0;
+    for (i = 0; i < *cont; i++) {
+        if (agenda[i].codigo > maior) {
+            maior = agenda[i].codigo;
+        }
+    }
+
+    return maior+1;
 }
